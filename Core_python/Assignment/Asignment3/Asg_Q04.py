163fdcc75cae8c6d6a73f4ff2bb8a96416e2bf77
purvahgorde <161198651+purvahgorde@users.noreply.github.com>
2026-07-23 17:31:54 +0530

    Delete Core_python/Assignment/Asignment3 directory

--- Core_python/Assignment/Asignment3/Asg_Q04.py
+++
@@ -1,8 +0,0 @@
-s1 = int(input('enter side1:'))
-s2 = int(input('enter side2:'))
-s3 = int(input('enter side3:'))
-
-if(s1+s2 > s3 and s2+s3 >s1 and s1+s3 >s2 ):
-    print('trangle is valid')
-else:
-    print('triangle is not valid')
