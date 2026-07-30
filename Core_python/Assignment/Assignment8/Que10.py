39a4cf99f117ae22df6fd8176230ee533858c745
purvahgorde <161198651+purvahgorde@users.noreply.github.com>
2026-07-30 22:22:27 +0530

    Delete Core_python/Assignment/Assignment8 directory

--- Core_python/Assignment/Assignment8/Que10.py
+++
@@ -1,8 +0,0 @@
-# Write a program to check if entered year is a leap year or not.
-def leapYear(year):
-    if((year % 400 ==0) or ( year % 4 ==0 and year % 100 !=0) ):
-        print(f'{year} is leap year')
-    else:
-        print(f'{year} is not leap year')
-
-leapYear(2024)
--- Core_python/Assignment/Assignment8/Que2.py
+++
@@ -1,8 +0,0 @@
-# Write a program to calculate area of circle
-import math
-def area_of_circle(r):
-    area =math.pi * r**2
-
-    print(f'the area of circle of is:{area:.2f}')
-
-area_of_circle(5)
